503f9e5fbc2a66d32d7430f2af7cf21241fcc18c
Brian <eyenani@gmail.com>
2024-10-18 15:20:20 -0600

    Exercise 4.8

--- controller/router.js
+++ controller/router.js
@@ -2,22 +2,17 @@ const route = require('express').Router()
 const Blog = require('../models/blog')
 
 
-route.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+route.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+
   })
   
-route.post('/', (request, response) => {
+route.post('/', async (request, response) => {
     const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    const result = await blog.save()
+    response.status(201).json(result)
+
   })
 
 
